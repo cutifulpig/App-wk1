27306138d51a0cf837237a2e54d7b00f54847b8a
cutifulpig <david0910932933@gmail.com>
2017-03-05 23:32:21 +0800

    1st

---
+++ 3.js
@@ -0,0 +1,15 @@
+var greetme=function(){
+  console.log(`hi tony!`);
+}
+greetme();
+
+
+greetsam=()=>console.log(`hi sam!`);
+greetsam();
+
+
+
+greetyou=name=>console.log(`hi ${name}!`);
+greetyou(`ntue`);
+
+
